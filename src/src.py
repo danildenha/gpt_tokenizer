21a8c7a8313a2d2225fc02c13bcd594cda59cc26
danildenha <danildenha@gmail.com>
2024-03-31 14:21:48 -0500

    creating new tokens from the current tokens list

--- src/src.py
+++ src/src.py
@@ -7,14 +7,14 @@ print(list("привіт, how are you?😁".encode("utf-8")))
 text = "ＵＵＵｎｉｃｏｄｅ!𝔥𝔢𝔱 𝔥𝔬𝔴  𝔞𝔯𝔢 𝔶𝔬𝔲. As of Unicode version 15.1, there are 149,878 characters with code points, covering 161 modern and historical scripts, as well as multiple symbol sets. 𝓐𝓘, 𝕒𝕝𝕤𝕠 𝕜𝕟𝕠𝕨𝕟 𝕒𝕤 𝔸𝕣𝕥𝕚𝕗𝕚𝕔𝕚𝕒𝕝 𝕀𝕟𝕥𝕖𝕝𝕝𝕚𝕘𝕖𝕟𝕔𝕖, 𝕚𝕤 𝕒 𝕓𝕣𝕒𝕟𝕔𝕙 𝕠𝕗 𝕔𝕠𝕞𝕡𝕦𝕥𝕖𝕣 𝕤𝕔𝕚𝕖𝕟𝕔𝕖 𝕕𝕖𝕕𝕚𝕔𝕒𝕥𝕖𝕕 𝕥𝕠 𝕞𝕒𝕜𝕖 𝕔𝕠𝕞𝕡𝕦𝕥𝕖𝕣𝕤 𝕒𝕟𝕕 𝕤𝕪𝕤𝕥𝕖𝕞𝕤 𝕥𝕙𝕒𝕥 𝕔𝕒𝕟 𝕓𝕖𝕙𝕒𝕧𝕖 𝕔𝕠𝕞𝕡𝕝𝕖𝕩 𝕚𝕥𝕤𝕖𝕝𝕗. 𝓞𝓷𝓮 𝓸𝓯 𝓽𝓱𝓮 𝓶𝓸𝓼𝓽 𝓭𝓲𝓼𝓽𝓲𝓷𝓬𝓽𝓲𝓿𝓮 𝓯𝓮𝓪𝓽𝓾𝓻𝓮𝓼 𝓸𝓯 𝓪𝓲 𝓲𝓼 𝓲𝓽𝓼 𝓬𝓪𝓹𝓪𝓫𝓲𝓵𝓲𝓽𝔂 𝓽𝓸 𝓵𝓮𝓪𝓻𝓷 𝓪𝓷𝓭 𝓪𝓭𝓪𝓹𝓽 𝓽𝓸 𝓷𝓮𝔀 𝓬𝓱𝓪𝓵𝓵𝓮𝓷𝓰𝓮𝓼. 𝓐𝓘 𝓪𝓵𝓰𝓸𝓻𝓲𝓽𝓱𝓶𝓼 𝓽𝓮𝓬𝓱𝓷𝓸𝓵𝓸𝓰𝔂, 𝓬𝓸𝓶𝓹𝓾𝓽𝓮𝓻 𝓿𝓲𝓼𝓲𝓸𝓷, 𝓷𝓪𝓽𝓾𝓻𝓪𝓵 𝓵𝓪𝓷𝓰𝓾𝓪𝓰𝓮 𝓹𝓻𝓸𝓬𝓮𝓼𝓼𝓲𝓷𝓰, 𝓪𝓷𝓭 𝓮𝓿𝓮𝓷 𝓼𝓮𝓷𝓽𝓲𝓶𝓮𝓷𝓽 𝓪𝓷𝓭 𝓮𝓶𝓸𝓽𝓲𝓸𝓷𝓪𝓵 𝓮𝔁𝓹𝓻𝓮𝓼𝓼𝓲𝓸𝓷. 𝓣𝓱𝓮 𝓯𝓾𝓽𝓾𝓻𝓮 𝓸𝓯 𝓐𝓘 𝓲𝓼 𝓯𝓪𝓻 𝓰𝓻𝓮𝓪𝓽𝓮𝓻 𝓹𝓻𝓸𝓬𝓮𝓼𝓼𝓲𝓷𝓰 𝓲𝓷𝓽𝓮𝓵𝓵𝓲𝓰𝓮𝓷𝓬𝓮. 𝓘𝓽 𝓲𝓷𝓬𝓵𝓾𝓭𝓮𝓼 𝓭𝓮𝓿𝓮𝓵𝓸𝓹𝓲𝓷𝓰 𝓪𝓵𝓰𝓸𝓻𝓲𝓽𝓱𝓶𝓼, 𝓿𝓲𝓻𝓽𝓾𝓪𝓵 𝓹𝓻𝓮𝓭𝓲𝓬𝓽𝓲𝓸𝓷, 𝓪𝓷𝓭 𝓬𝓻𝓮𝓪𝓽𝓲𝓿𝓲𝓽𝔂.𝓞𝓿𝓮𝓻 𝓽𝓱𝓮 𝓹𝓪𝓼𝓽 𝔂𝓮𝓪𝓻𝓼, 𝓐𝓘 𝓱𝓪𝓼 𝓮𝔁𝓬𝓮𝓵𝓵𝓮𝓭 𝓼𝓲𝓰𝓷𝓲𝓯𝓲𝓬𝓪𝓷𝓽𝓵𝔂, 𝓼𝓽𝓮𝓮𝓻𝓮𝓭, 𝓪𝓷𝓭 𝓮𝓿𝓮𝓷 𝓭𝓻𝓪𝓼𝓽𝓲𝓬𝓪𝓵𝓵𝔂 𝓲𝓶𝓹𝓻𝓸𝓿𝓮𝓭 𝓱𝓾𝓶𝓪𝓷 𝓮𝔁𝓹𝓮𝓻𝓲𝓮𝓷𝓬𝓮. 𝓘𝓽'𝓼 𝓪 - this is example of unicode values"
 tokens = text.encode("utf-8") # raw bytes
 tokens = list(map(int, tokens)) # convert to a list in range 0-255
-print('-------------')
-print(text)
-print('-------------')
-print("length: ", len(text))
-print('-------------')
-print(tokens)
-print("length of tokens: ", len(tokens)) #proving that tokens are longer than text
-print('-------------')
+#print('-------------')
+# print(text)
+# print('-------------')
+# print("length: ", len(text))
+# print('-------------')
+# print(tokens)
+# print("length of tokens: ", len(tokens)) #proving that tokens are longer than text
+# print('-------------')
 
 #Pair bitwyse encoding of tokens 
 def getPairs(tokens):
@@ -38,16 +38,17 @@ def merge(tokens, pair, newId):
 #merging the most frequent pairs of tokens
 new_tokens_number = 280
 number_iterations = new_tokens_number - 256
-tokens_copy = list(tokens)
+newTokens = list(tokens)
 
 merges = {}
 for i in range(number_iterations):
-    stats = getPairs(tokens_copy)
+    stats = getPairs(newTokens)
     pair = max(stats, key=stats.get)
     newId = 256 + i
     print("merging ", pair, " into a new token ", newId)
-    tokens_copy = merge(tokens_copy, pair, newId)
+    newTokens = merge(newTokens, pair, newId)
     merges[pair] = newId 
 
-print("length of tokens: ", len(merges)) #proving that tokens are longer than text
-print('-------------')
+print("tokens length:", len(tokens))
+print("newTokens length:", len(newTokens))
+print(f"compression ratio: {len(tokens) / len(newTokens):.2f}X")
