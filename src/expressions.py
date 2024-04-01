62a3fb919088edaca6b0704aef3d9e55f6753633
danildenha <danildenha@gmail.com>
2024-04-01 17:22:10 -0500

    adding tiktoken

--- src/expressions.py
+++ src/expressions.py
@@ -1,5 +1,11 @@
 #Practiving with regex from GPT-2
 import regex as re
+import tiktoken
+
+# GPT-2 (does not merge spaces)
+enc = tiktoken.get_encoding("gpt2")
+print(enc.encode("    hello world!!!"))
+
 gpt2pat = re.compile(r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
 
 print(re.findall(gpt2pat, "Hello've world123 how's are you!!!?"))
