eadd66e5ac72e6677edbbba0ec4d4080f8fa62c0
danildenha <danildenha@gmail.com>
2024-03-31 22:26:26 -0500

    decode algorithm for tokenizer

---
+++ src/decode.py
@@ -0,0 +1,13 @@
+from src import *
+
+vocab = {idx: bytes([idx]) for idx in range(256)}
+for (p0, p1), idx in merges.items():
+    vocab[idx] = vocab[p0] + vocab[p1]
+
+def decode(ids):
+  # given ids (list of integers), return Python string
+  tokens = b"".join(vocab[idx] for idx in ids)
+  text = tokens.decode("utf-8", errors="replace")
+  return text
+
+print(decode([97]))
